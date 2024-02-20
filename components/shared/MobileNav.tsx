05532459e7dd7ffba9b4d77c3440a3ce78eed813
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-20 19:40:18 +0100

    sheet

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,6 +1,17 @@
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import Image from 'next/image';
 const MobileNav = ()=>{
     return (
-        <div></div>
+        <nav className="md:hidden">
+            <Sheet>
+                <SheetTrigger className="align-middle">
+                    <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} className="cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                <Image src="/assets/icons/logo.svg" alt="logo" width={128} height={38} />
+                </SheetContent>
+            </Sheet>
+        </nav>
     )
 }
 export default MobileNav;
