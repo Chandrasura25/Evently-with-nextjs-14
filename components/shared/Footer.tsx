2d217e0f8adc0bf8d9ac30cab5ab74954ab9fd7d
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-22 13:09:00 +0100

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,22 @@
-const Footer = ()=>{
-    return (
-        <div></div>
-    )
-}
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
