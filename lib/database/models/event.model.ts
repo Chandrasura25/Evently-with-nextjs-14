d469d6b184d44fee665d42232ebd10e50af000bd
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-22 14:21:36 +0100

    database connection

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,20 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  deescription?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
+}
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
@@ -10,7 +26,7 @@ const EventSchema = new Schema({
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
-  utl: { type: String },
+  url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: "Category" },
   organizer: { type: Schema.Types.ObjectId, ref: "User" },
 });
