eb3676f47ed6aec835c5c5c7afc10d72688d75e6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-22 12:44:04 +0100

    components

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,7 @@
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { Separator } from '@/components/ui/separator';
 import Image from 'next/image';
+import NavItems from './NavItems';
 const MobileNav = ()=>{
     return (
         <nav className="md:hidden">
@@ -9,6 +11,8 @@ const MobileNav = ()=>{
                 </SheetTrigger>
                 <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
                 <Image src="/assets/icons/logo.svg" alt="logo" width={128} height={38} />
+                <Separator className="border border-gray-50" />
+                <NavItems />
                 </SheetContent>
             </Sheet>
         </nav>
